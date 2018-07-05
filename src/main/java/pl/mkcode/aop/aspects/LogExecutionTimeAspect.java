82101b682fb35222c3362a04c7e356cd7fa48c14
Marcin Kominek <marcin.kominek@digitalvirgo.pl>
2018-07-05 15:14:32 +0200

    checking @around annotation

--- src/main/java/pl/mkcode/aop/aspects/LogExecutionTimeAspect.java
+++ src/main/java/pl/mkcode/aop/aspects/LogExecutionTimeAspect.java
@@ -1,6 +1,9 @@
 package pl.mkcode.aop.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,12 +17,24 @@ public class LogExecutionTimeAspect {
     private static Logger logger = Logger.getLogger(LogExecutionTimeAspect.class.getName());
 
     @After("@annotation(pl.mkcode.aop.annotations.Logged)")
-    public void afterLogged() {
-        logger.info("after Logged");
+    public void afterLogged(JoinPoint jp) {
+        String methodName = jp.getSignature().getName();
+        logger.info(methodName + ": after Logged");
     }
 
     @Before("@annotation(pl.mkcode.aop.annotations.Logged)")
-    public void beforeLogged(){
-        logger.info("before Logged");
+    public void beforeLogged(JoinPoint jp) {
+        String methodName = jp.getSignature().getName();
+        logger.info(methodName + ": before Logged ");
+    }
+
+    @Around("@annotation(pl.mkcode.aop.annotations.Logged)")
+    public Object aroundLogged(ProceedingJoinPoint pjp) throws Throwable {
+        String methodName = pjp.getSignature().getName();
+        logger.info(methodName + ": around start");
+        Object retval = pjp.proceed();
+        logger.info(methodName + ": around end");
+
+        return retval;
     }
 }
--- src/test/java/pl/mkcode/aop/aspects/LogExecutionTimeAspectTest.java
+++ src/test/java/pl/mkcode/aop/aspects/LogExecutionTimeAspectTest.java
@@ -37,4 +37,11 @@ class LogExecutionTimeAspectTest {
 
         outputCapture.expect(stringContainsInOrder(asList("before Logged", "feeding", "after Logged")));
     }
+
+    @Test
+    void testBeforeAndAfterAnnotations(CaptureSystemOutput.OutputCapture outputCapture) {
+        zooService.feed(new Animal());
+
+        outputCapture.expect(stringContainsInOrder(asList("before Logged", "feeding", "after Logged")));
+    }
 }
